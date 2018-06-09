150ed6d96de49c0bc8b1019598d981f9abab8224
Edem Dumenu <ehdemdume@gmail.com>
2018-06-09 16:13:06 -0400

    Added a pointer to the application

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 $(function(){
-    
-   $("#slider").slider(); 
+    //Slider 
+   $("#slider").slider({
+     //Setting the min and max values
+     min: 3, 
+     max: 30,
+     slide: function(event, ui){
+         //accessin the value of the slider
+         $("#circle").height(ui.value);
+         $("#circle").width(ui.value);
+         
+     }   
+   }); 
     
 });
